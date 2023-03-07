1444301803c9d1b381d0cdb6960e430d12edfc07
Jalen <jalenr@techexchange.in>
2023-03-07 22:41:30 +0000

    Finished get_wiki_page and get_all_page_names

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,16 +1,28 @@
+from google.cloud import storage
+
 # TODO(Project 1): Implement Backend according to the requirements.
 class Backend:
 
     def __init__(self):
-        pass
+        self.storage_client = storage.Client()
+        self.pages = []
+        for blob in self.storage_client.list_blobs("wiki_content_p1"):
+            self.pages.append(blob.name.strip(".txt"))
+
         
-    def get_wiki_page(self, name):
-        pass
+    def get_wiki_page(self, name): #wiki_content_p1
+        bucket_name = "wiki_content_p1"
+        blob_name = name
+        bucket = self.storage_client.bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        
+        with blob.open("r") as f:
+            return f.read()
 
     def get_all_page_names(self):
-        pass
+        return self.pages
 
-    def upload(self):
+    def upload(self, blob):
         pass
 
     def sign_up(self):
@@ -22,3 +34,6 @@ class Backend:
     def get_image(self):
         pass
 
+backend = Backend()
+#print(backend.get_wiki_page("ginkgo.txt"))
+print(backend.get_all_page_names())
