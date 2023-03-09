ad7eda5644588e6aaf9f2bd1dcbb512fe87f029d
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-03-08 20:44:30 -0500

    Removed random fixture lol

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -19,10 +19,6 @@ def app():
 def client(app):
     return app.test_client()
 
-@pytest.fixture
-def backend():
-    return mock.MagicMock()
-
 def test_home_page(client):
     resp = client.get("/")
     assert resp.status_code == 200
