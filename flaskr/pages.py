186e21d143d5ce16cb4e1cc5ef65c6e6fb409ca7
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-04-01 23:54:54 -0400

    Fixed var name

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -71,7 +71,7 @@ def make_endpoints(app, backend):
 
         name = request.form['name']
         content_str = request.form['content']
-        if not content:
+        if not content_str:
             file = request.files['file']
             backend1.bucket_upload(name, file)
         else:
