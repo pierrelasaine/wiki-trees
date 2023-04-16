edcf1bf9f895a41605168bcd089c43fb2cb024dd
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-04-15 20:47:22 -0400

    Unit Test format

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from werkzeug.datastructures import FileStorage
 from flaskr.pages import *
 import io
@@ -8,8 +8,6 @@ import os
 import warnings
 
 
-# See https://flask.palletsprojects.com/en/2.2.x/testing/
-# for more info on testing
 @pytest.fixture
 def app():
     app = create_app({'TESTING': True, 'SECRET_KEY': 'test'})
@@ -23,12 +21,14 @@ def client(app):
 
 def test_home_page(client):
     resp = client.get("/")
+
     assert resp.status_code == 200
     assert b"home-box" in resp.data
 
 
 def test_about_page(client):
     resp = client.get("/about")
+
     assert resp.status_code == 200
     assert b"About Us!" in resp.data
 
@@ -38,11 +38,13 @@ def test_get_image(mock_get_image, client):
     mock_get_image.return_value = os.urandom(1024)
 
     resp = client.get("/images/mock_image")
+
     assert resp.status_code == 200
 
 
 def test_image_nonexistent(client):
     resp = client.get("/images/nonexistent")
+
     assert resp.status_code == 404
     assert b"Not Found" in resp.data
 
@@ -50,7 +52,9 @@ def test_image_nonexistent(client):
 @patch("flaskr.backend.Backend.get_wiki_page")
 def test_pages_wiki_nonexistent(mock_get_wiki_page, client):
     mock_get_wiki_page.return_value = None
+
     resp = client.get("/pages/nonexistent")
+
     assert resp.status_code == 404
     assert b"Not Found" in resp.data
 
@@ -60,12 +64,14 @@ def test_page_details(mock_get_wiki_page, client):
     mock_get_wiki_page.return_value = "<p>Mock Text for Unit Test</p>"
 
     resp = client.get("/pages/mock-page")
+
     assert resp.status_code == 200
     assert b"Mock Text for Unit Test" in resp.data
 
 
 def test_upload_page(client):
     resp = client.get("/upload")
+
     assert resp.status_code == 200
     assert b"Upload!" in resp.data
 
@@ -75,27 +81,35 @@ def test_upload_page(client):
 def test_TinyMCE_upload(mock_upload, mock_get_wiki_page, client):
     mock_upload.return_value = None
     mock_get_wiki_page.return_value = "Test HTML"
+
     resp = client.post("/upload",
                        data=dict(name="test_page", content="<p>Test HTML</p>"))
+
     assert resp.status_code == 302
+
     resp = client.get("/pages/test_page")
+
     assert resp.status_code == 200
     assert b"Test HTML" in resp.data
 
 
-"""
 @patch("flaskr.backend.Backend.get_wiki_page")
 @patch("flaskr.backend.Backend.upload")
 def test_file_upload(mock_upload, mock_get_wiki_page, client):
     mock_upload.return_value = None
     mock_get_wiki_page.return_value = "Test HTML"
+
     resp = client.post("/upload",
-                        data=dict(name="test_page",
-                                  fileb=FileStorage(filename="test.html", stream=b"<p>Test HTML</p>")))
+                       data=dict(name="test_page",
+                                 file=FileStorage(filename="test.html",
+                                                  stream=b"<p>Test HTML</p>")))
+
+    #assert resp.status_code == 302
+
     resp = client.get("/pages/test_page")
+
     assert resp.status_code == 200
     assert b"Test HTML" in resp.data
-"""
 
 
 def pytest_configure(config):
