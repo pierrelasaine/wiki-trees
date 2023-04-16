4eec325e8aba287cddb6fd84a0164ed7083724a0
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-04-16 14:03:12 -0400

    fixed quotes

--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -4,35 +4,35 @@
  * managing unsaved changes, handling the drawer's state, and adjusting margins for wiki pages.
  */
 
-/**
- * Toggle the editor's visibility and initialize or remove TinyMCE editor accordingly.
- */
 function toggleEditor() {
+    /**
+     * Toggle the editor's visibility and initialize or remove TinyMCE editor accordingly.
+     */
     // Get the editor container element
-    var editorContainer = document.getElementById('editor-container');
+    var editorContainer = document.getElementById("editor-container");
 
     // Check if editor container is not displayed
-    if (editorContainer.style.display === 'none') {
+    if (editorContainer.style.display === "none") {
         // If not displayed, display the container and initialize the TinyMCE editor
-        editorContainer.style.display = 'block';
+        editorContainer.style.display = "block";
         tinymce.init({
             height: 1000,
-            selector: '#myTextarea',
-            plugins: 'anchor autolink charmap codesample image link lists media searchreplace table visualblocks wordcount',
-            toolbar: 'export | undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+            selector: "#myTextarea",
+            plugins: "anchor autolink charmap codesample image link lists media searchreplace table visualblocks wordcount",
+            toolbar: "export | undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
             setup: function (editor) {
                 // Save initial content
                 var initialContent = editor.getContent();
             
                 // Add event listener for beforeunload event
-                window.addEventListener('beforeunload', handler, false);
-                window.addEventListener('click', handler, false);
+                window.addEventListener("beforeunload", handler, false);
+                window.addEventListener("click", handler, false);
 
                 function handler(event) {
                     // Check if content has been changed
                     if (initialContent !== editor.getContent()) {
                         // Display confirmation dialog
-                        var confirmationMessage = 'You have unsaved changes. Do you want to save them?';
+                        var confirmationMessage = "You have unsaved changes. Do you want to save them?";
                         (event || window.event).returnValue = confirmationMessage;
                         return confirmationMessage;
                     }
@@ -41,57 +41,57 @@ function toggleEditor() {
         });
     } else {
         // If displayed, hide the container and remove the TinyMCE editor
-        editorContainer.style.display = 'none';
+        editorContainer.style.display = "none";
         tinymce.remove();
     }
 }
 
-/**
- * Toggle the form's visibility and adjust the layout accordingly.
- */
 function toggleForm() {
-    var uploadBox = document.getElementById('upload-box-box')
-    var formContainer = document.getElementById('form-container');
-    var toggleButton = document.getElementById('toggleButton');
+    /**
+     * Toggle the form's visibility and adjust the layout accordingly.
+     */
+    var uploadBox = document.getElementById("upload-box-box")
+    var formContainer = document.getElementById("form-container");
+    var toggleButton = document.getElementById("toggleButton");
 
-    if (formContainer.style.display === 'block') {
-        formContainer.style.display = 'none';
-        uploadBox.style.marginTop = '40px';
+    if (formContainer.style.display === "block") {
+        formContainer.style.display = "none";
+        uploadBox.style.marginTop = "40px";
         toggleButton.innerHTML = "<img src='/static/images/back.png'>";
-        toggleButton.classList.add('upload-button-back')
+        toggleButton.classList.add("upload-button-back")
     } 
     else {
-        formContainer.style.display = 'block';
-        uploadBox.style.marginTop = '200px'
+        formContainer.style.display = "block";
+        uploadBox.style.marginTop = "200px"
         toggleButton.innerHTML = "Create New Page";
-        toggleButton.classList.remove('upload-button-back')
+        toggleButton.classList.remove("upload-button-back")
     }
 }
 
-/**
- * Toggle the visibility of the save button container.
- */
 function toggleUpload() {
-    var saveContainer = document.getElementById('save-button-container');
+    /**
+     * Toggle the visibility of the save button container.
+     */
+    var saveContainer = document.getElementById("save-button-container");
 
-    if (saveContainer.style.display === 'none') {
-        saveContainer.style.display = 'block';
+    if (saveContainer.style.display === "none") {
+        saveContainer.style.display = "block";
     } else {
-        saveContainer.style.display = 'none';
+        saveContainer.style.display = "none";
     }
 }
 
-/**
- * Verify if the user wants to leave the page with unsaved changes.
- * @return {boolean} to indicate whether to proceed with the default action.
- */
 function verifyLeaveFunction() {
+    /**
+     * Verify if the user wants to leave the page with unsaved changes.
+     * @return {boolean} to indicate whether to proceed with the default action.
+     */
     var editor = tinymce.get("myTextarea");
 
     // Check if content has been changed
     if (editor.isDirty()) {
         // Display confirmation dialog
-        var confirmationMessage = 'You have unsaved changes. Are you sure you want to leave?';
+        var confirmationMessage = "You have unsaved changes. Are you sure you want to leave?";
         if (!confirm(confirmationMessage)) {
         // User chose to cancel, so prevent default action
         return false;
@@ -101,132 +101,132 @@ function verifyLeaveFunction() {
   return window.history.back();
 }
 
-/**
- * Toggle the page data container's visibility and update the toggle button accordingly.
- */
 function togglePage() {
-    var formContainer = document.getElementById('page-data-container');
-    var toggleButton = document.getElementById('toggleButton');
+    /**
+     * Toggle the page data container's visibility and update the toggle button accordingly.
+     */
+    var formContainer = document.getElementById("page-data-container");
+    var toggleButton = document.getElementById("toggleButton");
 
-    if (formContainer.style.display === 'block') {
-        formContainer.style.display = 'none';
+    if (formContainer.style.display === "block") {
+        formContainer.style.display = "none";
         toggleButton.innerHTML = "<img src='/static/images/back.png'>";
         toggleButton.setAttribute("onclick", "verifyLeaveFunction()");
     } else {
-        formContainer.style.display = 'block';
+        formContainer.style.display = "block";
         toggleButton.innerHTML = "<img src='/static/images/edit.png'>";
         toggleButton.removeAttribute("onclick");
     }
 }
 
-/**
- * Toggle the visibility of the save button container.
- */
 function toggleSave() {
-    var saveContainer = document.getElementById('save-button-container');
+    /**
+     * Toggle the visibility of the save button container.
+     */
+    var saveContainer = document.getElementById("save-button-container");
 
-    if (saveContainer.style.display === 'none') {
-        saveContainer.style.display = 'block';
+    if (saveContainer.style.display === "none") {
+        saveContainer.style.display = "block";
     } else {
-        saveContainer.style.display = 'none';
+        saveContainer.style.display = "none";
     }
 }
 
-/**
- * Upload the content from the TinyMCE editor to the server by submitting the form.
- */
 function uploadPageFromEditor() {
+    /**
+     * Upload the content from the TinyMCE editor to the server by submitting the form.
+     */
     var myContent = tinymce.get("myTextarea").getContent();
 
     document.getElementById("contentInput").value = myContent;
     document.forms[0].submit();
 }
 
-/**
- * Toggle the drawer's visibility and store its state in sessionStorage.
- */
 function toggleDrawer() {
-    var pageDrawer = document.getElementById('toggleDrawer');
+    /**
+     * Toggle the drawer's visibility and store its state in sessionStorage.
+     */
+    var pageDrawer = document.getElementById("toggleDrawer");
 
-    if (pageDrawer.style.display === 'block') {
-        pageDrawer.style.display = 'none';
-        sessionStorage.setItem('drawerOpen', 'false');
+    if (pageDrawer.style.display === "block") {
+        pageDrawer.style.display = "none";
+        sessionStorage.setItem("drawerOpen", "false");
     } else {
-        pageDrawer.style.display = 'block';
-        sessionStorage.setItem('drawerOpen', 'true');
+        pageDrawer.style.display = "block";
+        sessionStorage.setItem("drawerOpen", "true");
     }
 }
 
-/**
- * Toggle the margin of the wiki page element.
- */
 function toggleWikiPageMargin() {
-    var wikiPage = document.querySelector('.wiki-page');
+    /**
+     * Toggle the margin of the wiki page element.
+     */
+    var wikiPage = document.querySelector(".wiki-page");
     
-    if (wikiPage.style.marginLeft === '0px') {
-        wikiPage.style.marginLeft = '250px';
+    if (wikiPage.style.marginLeft === "0px") {
+        wikiPage.style.marginLeft = "250px";
     } else {
-        wikiPage.style.marginLeft = '0px';
+        wikiPage.style.marginLeft = "0px";
     }
 }
 
 // Event listeners
 
-/**
- * Set the drawer's display state based on the value stored in sessionStorage.
- */
-document.addEventListener('DOMContentLoaded', function() {
-    var drawerOpen = sessionStorage.getItem('drawerOpen');
-    var pageDrawer = document.getElementById('toggleDrawer');
+document.addEventListener("DOMContentLoaded", function() {
+    /**
+     * Set the drawer's display state based on the value stored in sessionStorage.
+     */
+    var drawerOpen = sessionStorage.getItem("drawerOpen");
+    var pageDrawer = document.getElementById("toggleDrawer");
 
-    if (drawerOpen === 'true') {
-        pageDrawer.style.display = 'block';
+    if (drawerOpen === "true") {
+        pageDrawer.style.display = "block";
     } else {
-        pageDrawer.style.display = 'none';
+        pageDrawer.style.display = "none";
     }
 });
 
-/**
- * Hide the drawer if the unique-element is not present or does not have the data-default-drawer-open attribute.
- */
-document.addEventListener('DOMContentLoaded', function() {
-    var uniqueElement = document.querySelector('.unique-element');
+document.addEventListener("DOMContentLoaded", function() {
+    /**
+     * Hide the drawer if the unique-element is not present or does not have the data-default-drawer-open attribute.
+     */
+    var uniqueElement = document.querySelector(".unique-element");
 
-    if (!(uniqueElement && uniqueElement.hasAttribute('data-default-drawer-open'))) {
-        var pageDrawer = document.getElementById('toggleDrawer');
-        pageDrawer.style.display = 'none';
+    if (!(uniqueElement && uniqueElement.hasAttribute("data-default-drawer-open"))) {
+        var pageDrawer = document.getElementById("toggleDrawer");
+        pageDrawer.style.display = "none";
     }
 });
 
-/**
- * Show the drawer if the unique-element is present and has the data-default-drawer-open attribute.
- */
-document.addEventListener('DOMContentLoaded', function() {
-    var uniqueElement = document.querySelector('.unique-element');
+document.addEventListener("DOMContentLoaded", function() {
+    /**
+     * Show the drawer if the unique-element is present and has the data-default-drawer-open attribute.
+     */
+    var uniqueElement = document.querySelector(".unique-element");
 
-    if (uniqueElement && uniqueElement.hasAttribute('data-default-drawer-open')) {
-        var pageDrawer = document.getElementById('toggleDrawer');
-        pageDrawer.style.display = 'block';
+    if (uniqueElement && uniqueElement.hasAttribute("data-default-drawer-open")) {
+        var pageDrawer = document.getElementById("toggleDrawer");
+        pageDrawer.style.display = "block";
     }
 });
 
-/**
- * Toggle the drawer and wiki page margin when the window is resized, based on the unique-element and drawer state.
- */
-window.addEventListener('resize', function() {
-    var drawerOpen = sessionStorage.getItem('drawerOpen');
-    var uniqueElement = document.querySelector('.unique-element');
+window.addEventListener("resize", function() {
+    /**
+     * Toggle the drawer and wiki page margin when the window is resized, based on the unique-element and drawer state.
+     */
+    var drawerOpen = sessionStorage.getItem("drawerOpen");
+    var uniqueElement = document.querySelector(".unique-element");
 
-    if (window.matchMedia('(max-width: 768px)').matches && (uniqueElement && uniqueElement.hasAttribute('data-default-drawer-open')) && (drawerOpen === 'true')) {
+    if (window.matchMedia("(max-width: 768px)").matches && (uniqueElement && uniqueElement.hasAttribute("data-default-drawer-open")) && (drawerOpen === "true")) {
         toggleDrawer();
         toggleWikiPageMargin();
     }
 });
 
-/**
- * Hide the loader after the page has finished loading.
- */
 window.addEventListener("load", () => {
+    /**
+     * Hide the loader after the page has finished loading.
+     */
     const loader = document.querySelector(".loader");
     loader.classList.add("loader-hidden");
 
