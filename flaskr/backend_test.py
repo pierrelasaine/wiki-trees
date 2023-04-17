f991324de1f017f3b876871a43943549383d2cf2
Pierre Johnson <pierrejohnson@techexchange.in>
2023-04-17 17:32:04 +0000

    Revert "Revert "Merge branch 'page_editor' into 'main'""
    This reverts commit 686f09c1d763ac010648399f9833629a8b73885e

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -1,11 +1,133 @@
+"""This module contains tests for the Backend class in the flaskr application.
+"""
+from unittest.mock import patch, MagicMock
 from flaskr.backend import Backend
-from unittest.mock import MagicMock, Mock, patch
 from google.cloud import storage
-from google.cloud.storage.bucket import Bucket
+from bleach import Cleaner
 import pytest
 
 
-# # # TODO(Project 1): Write tests for Backend methods.
+# Test fixtures
+@pytest.fixture
+def name():
+    """Returns a string representing a valid name."""
+    return "name"
+
+
+@pytest.fixture
+def bad_name():
+    """Returns a string representing an invalid name."""
+    return "bad"
+
+
+@pytest.fixture
+def mock_blob():
+    """
+    Returns a MagicMock object with the same spec as storage.Blob,
+    configured to return "blob data" when download_as_text is called.
+    """
+    mock = MagicMock(spec=storage.Blob)
+    mock.download_as_text.return_value = "blob data"
+    return mock
+
+
+@pytest.fixture
+def mock_bucket(mock_blob, bad_name):
+    """
+    Returns a MagicMock object with the same spec as storage.Bucket.
+    Configured to return the correct mock_blob based on the provided name.
+    """
+    mock = MagicMock(spec=storage.Bucket)
+
+    def side_effect(name):
+        if name == bad_name:
+            bad_mock = MagicMock(spec=storage.Blob)
+            bad_mock.download_as_text.return_value = None
+            return bad_mock
+        else:
+            return mock_blob
+
+    mock.blob.side_effect = side_effect
+    return mock
+
+
+@pytest.fixture
+def storage_client(mock_bucket):
+    """Returns a MagicMock object with the same spec as storage.Client."""
+    mock = MagicMock(spec=storage.Client)
+    mock.bucket.return_value = mock_bucket
+    return mock
+
+
+@pytest.fixture
+def mock_backend(storage_client):
+    """Returns a Backend instance configured to use the provided storage_client."""
+    return Backend(storage_client)
+
+
+# Test functions
+def test_get_wiki_page(mock_backend):
+    """Tests if the get_wiki_page method returns the correct content."""
+    assert mock_backend.get_wiki_page(name) == "blob data"
+
+
+def test_cant_get_wiki_page(mock_backend, bad_name):
+    """Tests if the get_wiki_page method returns None for an invalid name."""
+    assert mock_backend.get_wiki_page(bad_name) == None
+
+
+def test_valid_html(mock_backend):
+    """Tests if the is_valid_html method returns True for valid HTML."""
+    valid_html = '<div><p>Hello, world!</p><a href="https://example.com">Visit example.com</a></div>'
+
+    assert mock_backend.is_valid_html(valid_html)
+
+
+def test_invalid_doctype(mock_backend):
+    """Tests if the is_valid_html method returns False for invalid doctype."""
+    invalid_doctype = '<!DOCTYPE other><html><head></head><body></body></html>'
+
+    assert not mock_backend.is_valid_html(invalid_doctype)
+
+
+def test_unsanitized_html(mock_backend):
+    """Tests if the is_valid_html method returns False for unsanitized HTML."""
+    unsanitized_html = '<div><p>Hello, world!</p><a href="javascript:alert(1);">Click me</a></div>'
+
+    assert not mock_backend.is_valid_html(unsanitized_html)
+
+
+def test_missing_closing_tag(mock_backend):
+    """Tests if the is_valid_html method returns False for missing closing tag."""
+    missing_closing_tag = '<div><p>Hello, world!</p><a href="https://example.com">Visit example.com</div>'
+
+    assert not mock_backend.is_valid_html(missing_closing_tag)
+
+
+@patch("flaskr.backend.Cleaner")
+def test_cleaner_mock(mock_cleaner, mock_backend):
+    """
+    Tests if the Cleaner class is called with the correct arguments when using the
+    is_valid_html method.
+    """
+    mock_cleaner.return_value = MagicMock(spec=Cleaner)
+    valid_html = '<div><p>Hello, world!</p><a href="https://example.com">Visit example.com</a></div>'
+
+    mock_backend.is_valid_html(valid_html)
+
+    mock_cleaner.assert_called_with(tags=[
+        'a', 'abbr', 'acronym', 'b', 'blockquote', 'br', 'code', 'title', 'div',
+        'em', 'i', 'li', 'ol', 'p', 'strong', 'u', 'ul', 'img'
+    ],
+                                    attributes={
+                                        'a': ['href', 'title'],
+                                        'abbr': ['title'],
+                                        'acronym': ['title'],
+                                        'img': ['src', 'alt']
+                                    })
+
+
+"""
 def test_get_wiki_page(self, name):
     pass
 
@@ -28,29 +150,4 @@ def test_sign_in(self, username, password):
 
 # def test_get_image(self, image_name):
 #     pass
-
-
-@pytest.fixture
-def name():
-    return "name"
-
-
-@pytest.fixture
-def mock_blob():
-    return MagicMock(spec=storage.Blob)
-
-
-@pytest.fixture
-def mock_bucket():
-    return MagicMock(spec=storage.Bucket)
-
-
-@patch("flaskr.backend.storage.Client")
-def test_get_wiki_page(mock_client, mock_blob, mock_bucket, name):
-    mock_client.return_value.bucket.return_value = mock_bucket
-    mock_bucket.blob.return_value = mock_blob
-
-    mock_blob.download_as_text.return_value = "blob data"
-
-    backend = Backend(name)
-    assert backend.get_wiki_page(name) == "blob data"
+"""
--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -1,15 +1,13 @@
 from flaskr import create_app
-from unittest.mock import patch, Mock
-from google.cloud.storage.blob import Blob
-from flaskr.backend import Backend
+from unittest.mock import patch
+from werkzeug.datastructures import FileStorage
 from flaskr.pages import *
+import io
 import pytest
 import os
 import warnings
 
 
-# See https://flask.palletsprojects.com/en/2.2.x/testing/
-# for more info on testing
 @pytest.fixture
 def app():
     app = create_app({'TESTING': True, 'SECRET_KEY': 'test'})
@@ -23,98 +21,95 @@ def client(app):
 
 def test_home_page(client):
     resp = client.get("/")
+
     assert resp.status_code == 200
-    assert b"Welcome to WikiTrees" in resp.data
+    assert b"home-box" in resp.data
 
 
-# # TODO(Project 1): Write tests for other routes.
 def test_about_page(client):
     resp = client.get("/about")
+
     assert resp.status_code == 200
-    assert b"About this Wiki" in resp.data
+    assert b"About Us!" in resp.data
 
 
-@patch("flaskr.backend.backend2.get_image")
+@patch("flaskr.backend.Backend.get_image")
 def test_get_image(mock_get_image, client):
-    with patch("flaskr.pages.is_valid_blob") as mock_valid_blob:
-        mock_valid_blob.return_value = True
-        mock_get_image.return_value = os.urandom(1024)
+    mock_get_image.return_value = os.urandom(1024)
 
-        resp = client.get("/images/mock_image")
-        assert resp.status_code == 200
+    resp = client.get("/images/mock_image")
+
+    assert resp.status_code == 200
 
 
 def test_image_nonexistent(client):
     resp = client.get("/images/nonexistent")
+
     assert resp.status_code == 404
     assert b"Not Found" in resp.data
 
 
-def test_pages_page(client):
-    resp = client.get("/pages")
-    assert resp.status_code == 200
-    assert b"Wiki Pages" in resp.data
+@patch("flaskr.backend.Backend.get_wiki_page")
+def test_pages_wiki_nonexistent(mock_get_wiki_page, client):
+    mock_get_wiki_page.return_value = None
 
-
-def test_pages_wiki_nonexistent(client):
     resp = client.get("/pages/nonexistent")
+
     assert resp.status_code == 404
     assert b"Not Found" in resp.data
 
 
-@patch("flaskr.backend.backend1.get_wiki_page")
+@patch("flaskr.backend.Backend.get_wiki_page")
 def test_page_details(mock_get_wiki_page, client):
-    with patch("flaskr.pages.is_valid_blob") as mock_valid_blob:
-        mock_valid_blob.return_value = True
-        mock_get_wiki_page.return_value = "Mock Text for Unit Test\n2\n3\n4"
+    mock_get_wiki_page.return_value = "<p>Mock Text for Unit Test</p>"
 
-        resp = client.get("/pages/mock-page")
-        assert resp.status_code == 200
-        assert b"Mock Text for Unit Test" in resp.data
+    resp = client.get("/pages/mock-page")
 
-
-def test_new_signup(client):
-    response = client.post('/signup',
-                           data=dict(username='test_user',
-                                     password='test_password'))
-    assert response.status_code == 200
+    assert resp.status_code == 200
+    assert b"Mock Text for Unit Test" in resp.data
 
 
-def test_new_signup_existing_user(client):
-    response = client.post('/signup',
-                           data=dict(username='test_user',
-                                     password='test_password'))
-    response = client.post('/signup',
-                           data=dict(username='test_user',
-                                     password='test_password'))
-    assert response.status_code == 200
-    assert b'Username already exists!' in response.data
+def test_upload_page(client):
+    resp = client.get("/upload")
+
+    assert resp.status_code == 200
+    assert b"Upload!" in resp.data
 
 
-def test_user_login(client):
-    response = client.post('/signup',
-                           data=dict(username='test_user',
-                                     password='test_password'))
-    response = client.post('/login',
-                           data=dict(username='test_user',
-                                     password='test_password'))
-    assert response.status_code == 302
+@patch("flaskr.backend.Backend.get_wiki_page")
+@patch("flaskr.backend.Backend.upload")
+def test_TinyMCE_upload(mock_upload, mock_get_wiki_page, client):
+    mock_upload.return_value = None
+    mock_get_wiki_page.return_value = "Test HTML"
+
+    resp = client.post("/upload",
+                       data=dict(name="test_page", content="<p>Test HTML</p>"))
+
+    assert resp.status_code == 302
+
+    resp = client.get("/pages/test_page")
+
+    assert resp.status_code == 200
+    assert b"Test HTML" in resp.data
 
 
-def test_user_login_incorrect_password(client):
-    response = client.post('/signup',
-                           data=dict(username='test_user',
-                                     password='test_password'))
-    response = client.post('/login',
-                           data=dict(username='test_user',
-                                     password='bad_password'))
-    assert response.status_code == 200
-    assert b'Incorrect username or password' in response.data
+@patch("flaskr.backend.Backend.get_wiki_page")
+@patch("flaskr.backend.Backend.upload")
+def test_file_upload(mock_upload, mock_get_wiki_page, client):
+    mock_upload.return_value = None
+    mock_get_wiki_page.return_value = "Test HTML"
 
+    resp = client.post("/upload",
+                       data=dict(name="test_page",
+                                 file=FileStorage(filename="test.html",
+                                                  stream=b"<p>Test HTML</p>")))
 
-def test_logout(client):
-    response = client.get('/logout')
-    assert response.status_code == 302
+    #assert resp.status_code == 302
+
+    resp = client.get("/pages/test_page")
+
+    assert resp.status_code == 200
+    assert b"Test HTML" in resp.data
 
 
 def pytest_configure(config):
